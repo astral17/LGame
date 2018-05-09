72ea6b9c589b91c06031915a6c494f63b407bbca
astral17 <2001.07.17dog@gmail.com>
2018-05-09 19:51:03 +0300

    init commit

---
+++ LGame/Bot.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace LGame
+{
+    public class Bot
+    {
+        Random rnd = new Random();
+        public int[,] Field = new int[4, 4]; // [x,y] 0 - White; 1 - Blue; 2 - Red; 3 - Yellow; 4 - Green
+        //Dictionary<ulong, bool> isWinning; // First(Blue) player Win? ; if second player has access to win pos -> first lose
+        Game LocalGame = new Game();
+        public enum Difficulties
+        {
+            Player = 0,
+            Random = 1,
+            Easy = 2, // if can finish game play, otherwise random
+            Medium = 3, // if can finish game play, otherwise not lose random
+            //Hard = 4, // 4 step, otherwise not lose random
+            //Perfect = 5, // theory of game, 90% of chance be the same as Hard
+        }
+        public Bot()
+        {
+
+        }
+        ulong Hash()
+        {
+            ulong hash = 0;
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    hash = ((hash * 7) % 1000000000000000007 + (ulong)Field[i, j] + 1) % 1000000000000000007;
+            return hash;
+        }
+        public void Run(Difficulties Difficulty = Difficulties.Random)
+        {
+            if (Difficulty == Difficulties.Random)
+            {
+                Player newPosition = new Player();
+                int x, y, f;
+                do
+                {
+                    f = rnd.Next(0, 7);
+                    x = rnd.Next(0, 3);
+                    y = rnd.Next(0, 3);
+                    for (int i = 0; i < 4; i++)
+                        newPosition[i] = new Point(Form1.SelfRef.game.LForms[f][i].X + x, Form1.SelfRef.game.LForms[f][i].Y + y);
+                } while (!Form1.SelfRef.game.CheckCorrect(newPosition));
+                Form1.SelfRef.game.Play(newPosition);
+                do
+                {
+                    f = rnd.Next(0, 1);
+                    x = rnd.Next(0, 3);
+                    y = rnd.Next(0, 3);
+                } while (!Form1.SelfRef.game.CheckStone(f, new Point(x, y)));
+                Form1.SelfRef.game.Play(f, new Point(x, y));
+                return;
+            }
+            if (Difficulty == Difficulties.Easy)
+            {
+                Player newPosition = new Player();
+                int x, y, f;
+                for (x = 0; x < 4; x++)
+                    for (y = 0; y < 4; y++)
+                        for (int i = 0; i < 8; i++)
+                        {
+                            LocalGame = new Game((Form1.SelfRef.game.IsStep(0) ? 0 : 1), Form1.SelfRef.game.player[0], Form1.SelfRef.game.player[1], Form1.SelfRef.game.stone[0], Form1.SelfRef.game.stone[1]);
+                            for (int j = 0; j < 4; j++)
+                                newPosition[j] = new Point(LocalGame.LForms[i][j].X + x, LocalGame.LForms[i][j].Y + y);
+                            if (LocalGame.CheckCorrect(newPosition))
+                            {
+                                //LocalGame.Play(newPosition);
+                                for (int i1 = 0; i1 < 4; i1++)
+                                    for (int j1 = 0; j1 < 4; j1++)
+                                        Field[i1, j1] = LocalGame.Field[i1, j1];
+                                for (int stoneID = 0; stoneID < 2; stoneID++)
+                                    for (int stoneX = 0; stoneX < 4; stoneX++)
+                                        for (int stoneY = 0; stoneY < 4; stoneY++)
+                                        {
+                                            LocalGame = new Game((Form1.SelfRef.game.IsStep(0) ? 0 : 1), Form1.SelfRef.game.player[0], Form1.SelfRef.game.player[1], Form1.SelfRef.game.stone[0], Form1.SelfRef.game.stone[1]);
+                                            LocalGame.Play(newPosition);
+                                            if (LocalGame.CheckStone(stoneID, new Point(stoneX, stoneY)))
+                                            {
+                                                LocalGame.Play(stoneID, new Point(stoneX, stoneY));
+                                                if (LocalGame.IsFinish())
+                                                {
+                                                    Form1.SelfRef.game.Play(newPosition, stoneID, new Point(stoneX, stoneY));
+                                                    return;
+                                                }
+                                                for (int i1 = 0; i1 < 4; i1++)
+                                                    for (int j1 = 0; j1 < 4; j1++)
+                                                        LocalGame.Field[i1, j1] = Field[i1, j1];
+                                                LocalGame.NextStep();
+                                            }
+                                        }
+                                for (int i1 = 0; i1 < 4; i1++)
+                                    for (int j1 = 0; j1 < 4; j1++)
+                                        LocalGame.Field[i1, j1] = Form1.SelfRef.game.Field[i1, j1];
+                            }
+                        }
+                do
+                {
+                    f = rnd.Next(0, 7);
+                    x = rnd.Next(0, 3);
+                    y = rnd.Next(0, 3);
+                    for (int i = 0; i < 4; i++)
+                        newPosition[i] = new Point(Form1.SelfRef.game.LForms[f][i].X + x, Form1.SelfRef.game.LForms[f][i].Y + y);
+                } while (!Form1.SelfRef.game.CheckCorrect(newPosition));
+                Form1.SelfRef.game.Play(newPosition);
+                do
+                {
+                    f = rnd.Next(0, 1);
+                    x = rnd.Next(0, 3);
+                    y = rnd.Next(0, 3);
+                } while (!Form1.SelfRef.game.CheckStone(f, new Point(x, y)));
+                Form1.SelfRef.game.Play(f, new Point(x, y));
+                return;
+            }
+            if (Difficulty == Difficulties.Medium)
+            {
+                Player newPosition = new Player();
+                int x, y, f, cnt = 10000;
+                for (x = 0; x < 4; x++)
+                    for (y = 0; y < 4; y++)
+                        for (int i = 0; i < 8; i++)
+                        {
+                            LocalGame = new Game((Form1.SelfRef.game.IsStep(0) ? 0 : 1), Form1.SelfRef.game.player[0], Form1.SelfRef.game.player[1], Form1.SelfRef.game.stone[0], Form1.SelfRef.game.stone[1]);
+                            for (int j = 0; j < 4; j++)
+                                newPosition[j] = new Point(LocalGame.LForms[i][j].X + x, LocalGame.LForms[i][j].Y + y);
+                            if (LocalGame.CheckCorrect(newPosition))
+                            {
+                                //LocalGame.Play(newPosition);
+                                for (int i1 = 0; i1 < 4; i1++)
+                                    for (int j1 = 0; j1 < 4; j1++)
+                                        Field[i1, j1] = LocalGame.Field[i1, j1];
+                                for (int stoneID = 0; stoneID < 2; stoneID++)
+                                    for (int stoneX = 0; stoneX < 4; stoneX++)
+                                        for (int stoneY = 0; stoneY < 4; stoneY++)
+                                        {
+                                            LocalGame = new Game((Form1.SelfRef.game.IsStep(0) ? 0 : 1), Form1.SelfRef.game.player[0], Form1.SelfRef.game.player[1], Form1.SelfRef.game.stone[0], Form1.SelfRef.game.stone[1]);
+                                            LocalGame.Play(newPosition);
+                                            if (LocalGame.CheckStone(stoneID, new Point(stoneX, stoneY)))
+                                            {
+                                                LocalGame.Play(stoneID, new Point(stoneX, stoneY));
+                                                if (LocalGame.IsFinish())
+                                                {
+                                                    Form1.SelfRef.game.Play(newPosition, stoneID, new Point(stoneX, stoneY));
+                                                    return;
+                                                }
+                                                for (int i1 = 0; i1 < 4; i1++)
+                                                    for (int j1 = 0; j1 < 4; j1++)
+                                                        LocalGame.Field[i1, j1] = Field[i1, j1];
+                                                LocalGame.NextStep();
+                                            }
+                                        }
+                                for (int i1 = 0; i1 < 4; i1++)
+                                    for (int j1 = 0; j1 < 4; j1++)
+                                        LocalGame.Field[i1, j1] = Form1.SelfRef.game.Field[i1, j1];
+                            }
+                        }
+                do
+                {
+                    f = rnd.Next(0, 7);
+                    x = rnd.Next(0, 3);
+                    y = rnd.Next(0, 3);
+                    for (int i = 0; i < 4; i++)
+                        newPosition[i] = new Point(Form1.SelfRef.game.LForms[f][i].X + x, Form1.SelfRef.game.LForms[f][i].Y + y);
+                    if (Form1.SelfRef.game.CheckCorrect(newPosition))
+                    {
+                        LocalGame = new Game((Form1.SelfRef.game.IsStep(0) ? 0 : 1), Form1.SelfRef.game.player[0], Form1.SelfRef.game.player[1], Form1.SelfRef.game.stone[0], Form1.SelfRef.game.stone[1]);
+                        LocalGame.Play(newPosition);
+                        do
+                        {
+                            f = rnd.Next(0, 1);
+                            x = rnd.Next(0, 3);
+                            y = rnd.Next(0, 3);
+                        } while (!LocalGame.CheckStone(f, new Point(x, y)));
+                        LocalGame.Play(f, new Point(x, y));
+                        if (LocalGame.IsFinish())
+                        {
+                            cnt--;
+                            continue;
+                        }
+                        Form1.SelfRef.game.Play(newPosition, f, new Point(x, y));
+                        return;
+                    }
+                    cnt--;
+                } while (cnt > 0);
+                // if all bad
+                newPosition = new Player();
+                do
+                {
+                    f = rnd.Next(0, 7);
+                    x = rnd.Next(0, 3);
+                    y = rnd.Next(0, 3);
+                    for (int i = 0; i < 4; i++)
+                        newPosition[i] = new Point(Form1.SelfRef.game.LForms[f][i].X + x, Form1.SelfRef.game.LForms[f][i].Y + y);
+                } while (!Form1.SelfRef.game.CheckCorrect(newPosition));
+                Form1.SelfRef.game.Play(newPosition);
+                do
+                {
+                    f = rnd.Next(0, 1);
+                    x = rnd.Next(0, 3);
+                    y = rnd.Next(0, 3);
+                } while (!Form1.SelfRef.game.CheckStone(f, new Point(x, y)));
+                Form1.SelfRef.game.Play(f, new Point(x, y));
+                return;
+            }
+        }
+    }
+}
---
+++ LGame/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LGame
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 SelfRef;
+        public readonly int CellSize = 150;
+        public Game game = new Game();
+        public Bot bot = new Bot();
+
+        private Player selected = new Player();
+        private int SelectedStone = -1;
+        private Phase StepPhase = Phase.MovePlayer;
+        private bool isMouseDown = false;
+
+        public enum Phase
+        {
+            MovePlayer = 0,
+            SelectStone = 1,
+            MoveStone = 2,
+            Finished = 3,
+        }
+        public Form1()
+        {            
+            InitializeComponent();
+            SelfRef = this;
+            selected.Clear();
+            game.RegisterBot(1);
+            timer1.Interval = 10;
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            /*Player tmp = new Player();
+            tmp[0] = new Point(2, 0);
+            tmp[1] = new Point(2, 1);
+            tmp[2] = new Point(2, 2);
+            tmp[3] = new Point(3, 2);
+            game.Play(tmp);*/
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Threading.Monitor.Enter(e.Graphics);
+            // Draw Map
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    switch (game.Field[i, j])
+                    {
+                        case 0:
+                            e.Graphics.FillRectangle(Brushes.White, i * CellSize, j * CellSize, CellSize, CellSize);
+                            break;
+                        case 1:
+                            e.Graphics.FillRectangle(Brushes.Blue, i * CellSize, j * CellSize, CellSize, CellSize);
+                            break;
+                        case 2:
+                            e.Graphics.FillRectangle(Brushes.Red, i * CellSize, j * CellSize, CellSize, CellSize);
+                            break;
+                        case 3:
+                            if (SelectedStone == 0)
+                            {
+                                if (game.IsPlayerStep(0))
+                                    e.Graphics.FillRectangle(Brushes.LightBlue, i * CellSize, j * CellSize, CellSize, CellSize);
+                                else
+                                    e.Graphics.FillRectangle(Brushes.Pink, i * CellSize, j * CellSize, CellSize, CellSize);
+                            }
+                            else
+                                e.Graphics.FillRectangle(Brushes.White, i * CellSize, j * CellSize, CellSize, CellSize);
+                            e.Graphics.FillEllipse(Brushes.Yellow, i * CellSize, j * CellSize, CellSize, CellSize);
+                            break;
+                        case 4:
+                            if (SelectedStone == 1)
+                            {
+                                if (game.IsPlayerStep(0))
+                                    e.Graphics.FillRectangle(Brushes.LightBlue, i * CellSize, j * CellSize, CellSize, CellSize);
+                                else
+                                    e.Graphics.FillRectangle(Brushes.Pink, i * CellSize, j * CellSize, CellSize, CellSize);
+                            }
+                            else
+                                e.Graphics.FillRectangle(Brushes.White, i * CellSize, j * CellSize, CellSize, CellSize);
+                            e.Graphics.FillEllipse(Brushes.Green, i * CellSize, j * CellSize, CellSize, CellSize);
+                            break;
+                    }
+                    e.Graphics.DrawRectangle(Pens.Black, i * CellSize, j * CellSize, CellSize, CellSize);
+                }
+            }
+            // Draw Selected
+            for (int i = 0; i < 4; i++)
+            {
+                if (0 <= selected[i].X && selected[i].X < 4 && 0 <= selected[i].Y && selected[i].Y < 4)
+                {
+                    if (game.IsPlayerStep(0))
+                    {
+                        e.Graphics.FillRectangle(Brushes.LightBlue, selected[i].X * CellSize, selected[i].Y * CellSize, CellSize, CellSize);
+                        e.Graphics.DrawRectangle(Pens.Black, selected[i].X * CellSize, selected[i].Y * CellSize, CellSize, CellSize);
+                    }
+                    if (game.IsPlayerStep(1))
+                    {
+                        e.Graphics.FillRectangle(Brushes.Pink, selected[i].X * CellSize, selected[i].Y * CellSize, CellSize, CellSize);
+                        e.Graphics.DrawRectangle(Pens.Black, selected[i].X * CellSize, selected[i].Y * CellSize, CellSize, CellSize);
+                    }
+                }
+            }
+            // Congratulations
+            if (StepPhase == Phase.Finished)
+            {
+                if (game.IsStep(0))
+                    e.Graphics.DrawString("Congratulations!", new Font("Arial", 32), Brushes.Pink, pictureBox1.Width / 2 - 180, pictureBox1.Height / 2 - 24);
+                if (game.IsStep(1))
+                    e.Graphics.DrawString("Congratulations!", new Font("Arial", 32), Brushes.LightBlue, pictureBox1.Width / 2 - 180, pictureBox1.Height / 2 - 24);
+                System.Threading.Monitor.Exit(e.Graphics);
+            }
+        }
+
+        private void DrawNewPosition(Point newPoint)
+        {
+            if (0 > newPoint.X || newPoint.X >= 4 || 0 > newPoint.Y || newPoint.Y >= 4)
+                return;
+            for (int i = 0; i < 4; i++)
+            {
+                if (selected[i] == newPoint)
+                    return;
+                if (selected[i].X == -1 && selected[i].Y == -1)
+                {
+                    selected[i] = newPoint;
+                    return;
+                }
+            }
+            selected.Clear();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point newPoint;
+            switch (StepPhase)
+            {
+                case Phase.MovePlayer:
+                    isMouseDown = true;
+                    newPoint = new Point(e.X / CellSize, e.Y / CellSize);
+                    DrawNewPosition(newPoint);
+                    break;
+                case Phase.SelectStone:
+                    //newPoint = new Point(e.X / CellSize, e.Y / CellSize);
+
+                    break;
+                case Phase.MoveStone:
+
+                    break;
+            }
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point newPoint = new Point(e.X / CellSize, e.Y / CellSize);
+                DrawNewPosition(newPoint);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point newPoint;
+            switch (StepPhase)
+            {
+                case Phase.MovePlayer:
+                    if (game.Play(selected))
+                        StepPhase = Phase.SelectStone;
+                    selected.Clear();
+                    isMouseDown = false;
+                    break;
+                case Phase.SelectStone:
+                    newPoint = new Point(e.X / CellSize, e.Y / CellSize);
+                    if (0 > newPoint.X || newPoint.X >= 4 || 0 > newPoint.Y || newPoint.Y >= 4)
+                        return;
+                    if (game.Field[newPoint.X, newPoint.Y] > 2)
+                    {
+                        SelectedStone = game.Field[newPoint.X, newPoint.Y] - 3;
+                        StepPhase = Phase.MoveStone;
+                    }
+                    break;
+                case Phase.MoveStone:
+                    newPoint = new Point(e.X / CellSize, e.Y / CellSize);
+                    if (0 > newPoint.X || newPoint.X >= 4 || 0 > newPoint.Y || newPoint.Y >= 4)
+                        return;
+                    if (game.Play(SelectedStone, newPoint))
+                    {
+                        SelectedStone = -1;
+                        StepPhase = Phase.MovePlayer;
+                        if (game.IsFinish())
+                            StepPhase = Phase.Finished;
+                    }
+                    break;
+            }
+        }
+    }
+}
